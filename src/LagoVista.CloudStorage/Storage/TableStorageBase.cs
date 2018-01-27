4882b61dc2df6e22b61079b6d21b35fc8ac71cdf
Kevin D Wolf <kevin@DESKTOP-R4064KP.(none)>
2018-01-27 07:20:37 -0500

    fix cloud service table storage paging

--- src/LagoVista.CloudStorage/Storage/TableStorageBase.cs
+++ src/LagoVista.CloudStorage/Storage/TableStorageBase.cs
@@ -753,14 +753,17 @@ namespace LagoVista.CloudStorage.Storage
             var query = $"?$filter=PartitionKey eq '{partitionKey}'";
             if (count.HasValue)
             {
-                query += $"$top={count.Value}";
+                query += $"&$top={count.Value}";
             }
 
+            //TODO: Add support for continuation tokens
+
+            /*
             //HACK: Is not effective since the record count could change, need to pass in the last value from the previous mechanism
             if (skip.HasValue)
             {
-                query += $"$skip={skip.Value}";
-            }
+                query += $"&$skip={skip.Value}";
+            }*/
 
             var operationUri = new Uri($"{_srvrPath}{resource}{query}");
 
