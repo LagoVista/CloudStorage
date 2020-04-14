69a2701a5bdcfe19c4ffcbd48c5a8158c1441cff
Kevin D. Wolf <kevinw@software-logistics.com>
2020-04-14 12:28:46 -0400

    Success Updated Newtonsoft.Json to 12.0.3

--- src/LagoVista.CloudStorage/Storage/CacheProvider.cs
+++ src/LagoVista.CloudStorage/Storage/CacheProvider.cs
@@ -11,29 +11,35 @@ namespace LagoVista.CloudStorage.Storage
 
         public CacheProvider(ICacheProviderSettings settings)
         {
-             _multiplexer = ConnectionMultiplexer.Connect(settings.CacheSettings.Uri);
+             //_multiplexer = ConnectionMultiplexer.Connect(settings.CacheSettings.Uri);
         }
 
         public Task AddAsync(string key, string value)
         {
-            Console.WriteLine($"Added cache item: {key}");
+            /*Console.WriteLine($"Added cache item: {key}");
             var db = _multiplexer.GetDatabase();
-            return db.StringSetAsync(key, value);
+            return db.StringSetAsync(key, value);*/
+
+            return Task.CompletedTask;
         }
 
-        public async Task<string> GetAsync(string key)
+        public Task<string> GetAsync(string key)
         {
-            var db = _multiplexer.GetDatabase();
+            /*var db = _multiplexer.GetDatabase();
             var result = await db.StringGetAsync(key);
 
             Console.WriteLine($"Getting cache item: {key} doest not exists: {String.IsNullOrEmpty(result)}");
-            return (string)result;
+            return (string)result;*/
+
+            return Task<string>.FromResult(default(string));
         }
 
         public Task RemoveAsync(string key)
         {
-            var db = _multiplexer.GetDatabase();
+            /*var db = _multiplexer.GetDatabase();
             return db.KeyDeleteAsync(key);
+            */
+            return Task.CompletedTask;
         }
     }
 }
