5c6ae6121d087c387811c4a05a2f36940781061d
Kevin D. Wolf <kevinw@software-logistics.com>
2026-08-10 15:09:10 -0400

    Add semantic registry metadata DTO

---
+++ src/LagoVista.Relational/SemanticRegistryDTO.cs
@@ -0,0 +1,45 @@
+using LagoVista.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LagoVista.Relational
+{
+    [Table("Registry", Schema = "dbo")]
+    public class SemanticRegistryDTO
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public long Revision { get; set; }
+
+        public long? SeededFromRevision { get; set; }
+
+        public DateTime? SeededUtc { get; set; }
+
+        public DateTime CreatedUtc { get; set; }
+
+        public DateTime UpdatedUtc { get; set; }
+
+        public static void Configure(ModelBuilder modelBuilder)
+        {
+            var provider = modelBuilder.GetProviderName();
+            var entity = modelBuilder.Entity<SemanticRegistryDTO>();
+
+            entity.HasKey(x => x.Id);
+            entity.HasIndex(x => x.Name).IsUnique().HasDatabaseName("UX_Registry_Name");
+
+            entity.Property(x => x.Id).HasColumnOrder(1).HasColumnType(StandardDBTypes.UuidStorage(provider));
+            entity.Property(x => x.Name).HasColumnOrder(2).HasColumnType(StandardDBTypes.NameStorage(provider));
+            entity.Property(x => x.Revision).HasColumnOrder(3).HasColumnType(StandardDBTypes.LongStorage(provider));
+            entity.Property(x => x.SeededFromRevision).HasColumnOrder(4).HasColumnType(StandardDBTypes.LongStorage(provider));
+            entity.Property(x => x.SeededUtc).HasColumnOrder(5).HasColumnType(StandardDBTypes.UtcTimestampStorage(provider));
+            entity.Property(x => x.CreatedUtc).HasColumnOrder(6).HasColumnType(StandardDBTypes.UtcTimestampStorage(provider));
+            entity.Property(x => x.UpdatedUtc).HasColumnOrder(7).HasColumnType(StandardDBTypes.UtcTimestampStorage(provider));
+        }
+    }
+}
