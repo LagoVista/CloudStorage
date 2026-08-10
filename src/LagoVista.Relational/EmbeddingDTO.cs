c7982d609f92b6ca3ab6ec6bd089c95b13628d05
Kevin D. Wolf <kevinw@software-logistics.com>
2026-08-10 16:25:53 -0400

    Add semantic owner organization id

--- src/LagoVista.Relational/EmbeddingDTO.cs
+++ src/LagoVista.Relational/EmbeddingDTO.cs
@@ -13,6 +13,9 @@ namespace LagoVista.Relational
         public Guid Id { get; set; }
 
         [Required]
+        public string OwnerOrganizationId { get; set; }
+
+        [Required]
         public string DefinitionId { get; set; }
 
         [Required]
@@ -34,23 +37,25 @@ namespace LagoVista.Relational
             var entity = modelBuilder.Entity<EmbeddingDTO>();
 
             entity.HasKey(x => x.Id);
+            entity.HasIndex(x => x.OwnerOrganizationId).HasDatabaseName("IX_Embeddings_OwnerOrganizationId");
             entity.HasIndex(x => new { x.DefinitionId, x.ModelKey }).IsUnique().HasDatabaseName("UX_Embeddings_DefinitionId_ModelKey");
             entity.HasIndex(x => x.SourceSha256).HasDatabaseName("IX_Embeddings_SourceSha256");
 
             entity.HasOne<DefinitionDTO>().WithMany().HasForeignKey(x => x.DefinitionId).OnDelete(DeleteBehavior.Cascade);
 
             entity.Property(x => x.Id).HasColumnOrder(1).HasColumnType(StandardDBTypes.UuidStorage(provider));
-            entity.Property(x => x.DefinitionId).HasColumnOrder(2).HasColumnType(StandardDBTypes.NormalizedId32Storage(provider));
-            entity.Property(x => x.ModelKey).HasColumnOrder(3).HasColumnType(StandardDBTypes.TextShort(provider));
-            entity.Property(x => x.Dimensions).HasColumnOrder(4).HasColumnType(StandardDBTypes.IntStorage(provider));
-            entity.Property(x => x.SourceSha256).HasColumnOrder(5).HasColumnType(StandardDBTypes.KeyStorage(provider));
-            entity.Property(x => x.Vector).HasColumnOrder(6).HasColumnType(provider switch
+            entity.Property(x => x.OwnerOrganizationId).HasColumnOrder(2).HasColumnType("varchar(32)").HasMaxLength(32);
+            entity.Property(x => x.DefinitionId).HasColumnOrder(3).HasColumnType(StandardDBTypes.NormalizedId32Storage(provider));
+            entity.Property(x => x.ModelKey).HasColumnOrder(4).HasColumnType(StandardDBTypes.TextShort(provider));
+            entity.Property(x => x.Dimensions).HasColumnOrder(5).HasColumnType(StandardDBTypes.IntStorage(provider));
+            entity.Property(x => x.SourceSha256).HasColumnOrder(6).HasColumnType(StandardDBTypes.KeyStorage(provider));
+            entity.Property(x => x.Vector).HasColumnOrder(7).HasColumnType(provider switch
             {
                 ModelBuilderProviderExtensions.Sqlite => "BLOB",
                 ModelBuilderProviderExtensions.Postgres => "bytea",
                 _ => "varbinary(max)"
             });
-            entity.Property(x => x.GeneratedUtc).HasColumnOrder(7).HasColumnType(StandardDBTypes.UtcTimestampStorage(provider));
+            entity.Property(x => x.GeneratedUtc).HasColumnOrder(8).HasColumnType(StandardDBTypes.UtcTimestampStorage(provider));
         }
     }
 }
