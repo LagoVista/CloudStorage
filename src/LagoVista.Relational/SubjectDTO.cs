00d3c937340a0919b30db75bfcae6035cdb2b3f1
Kevin D. Wolf <kevinw@software-logistics.com>
2026-08-10 15:09:21 -0400

    Add semantic subject DTO

---
+++ src/LagoVista.Relational/SubjectDTO.cs
@@ -0,0 +1,46 @@
+using LagoVista.Models;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LagoVista.Relational
+{
+    [Table("Subjects", Schema = "dbo")]
+    public class SubjectDTO
+    {
+        [Key]
+        public string Id { get; set; }
+
+        [Required]
+        public string Key { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public string StatusKey { get; set; }
+
+        public System.DateTime CreatedUtc { get; set; }
+
+        public System.DateTime UpdatedUtc { get; set; }
+
+        public static void Configure(ModelBuilder modelBuilder)
+        {
+            var provider = modelBuilder.GetProviderName();
+            var entity = modelBuilder.Entity<SubjectDTO>();
+
+            entity.HasKey(x => x.Id);
+            entity.HasIndex(x => x.Key).IsUnique().HasDatabaseName("UX_Subjects_Key");
+
+            entity.Property(x => x.Id).HasColumnOrder(1).HasColumnType(StandardDBTypes.NormalizedId32Storage(provider));
+            entity.Property(x => x.Key).HasColumnOrder(2).HasColumnType(StandardDBTypes.KeyStorage(provider));
+            entity.Property(x => x.Name).HasColumnOrder(3).HasColumnType(StandardDBTypes.NameStorage(provider));
+            entity.Property(x => x.Description).HasColumnOrder(4).HasColumnType(StandardDBTypes.TextMedium(provider));
+            entity.Property(x => x.StatusKey).HasColumnOrder(5).HasColumnType(StandardDBTypes.StatusStorage(provider));
+            entity.Property(x => x.CreatedUtc).HasColumnOrder(6).HasColumnType(StandardDBTypes.UtcTimestampStorage(provider));
+            entity.Property(x => x.UpdatedUtc).HasColumnOrder(7).HasColumnType(StandardDBTypes.UtcTimestampStorage(provider));
+        }
+    }
+}
