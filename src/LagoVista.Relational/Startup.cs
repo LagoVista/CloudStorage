28a3b5d2516ba4df16e504e693c1b1d7f970e5ef
Kevin D. Wolf <kevinw@software-logistics.com>
2026-08-10 15:10:25 -0400

    Configure live and test semantic databases

--- src/LagoVista.Relational/Startup.cs
+++ src/LagoVista.Relational/Startup.cs
@@ -8,50 +8,74 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.IdentityModel.Protocols.Configuration;
-using Microsoft.VisualBasic;
 using System;
-using System.Runtime.CompilerServices;
 
 namespace LagoVista.Relational
 {
     public static class Startup
     {
-        public static void ConfigureDataContextServices(IConfigurationRoot configurationRoot, Microsoft.Extensions.DependencyInjection.IServiceCollection services, ILogger logger)
+        public static void ConfigureDataContextServices(IConfigurationRoot configurationRoot, IServiceCollection services, ILogger logger)
         {
-            var section = configurationRoot.GetSection("BillingDb");
-
-            var connectionSettings = new ConnectionSettings()
+            var billingSection = configurationRoot.GetSection("BillingDb");
+            var billingConnectionSettings = new ConnectionSettings()
             {
-                Uri = section.Require("ServerURL"),
-                ResourceName = section.Require("InitialCatalog"),
-                UserName = section.Require("UserName"),
-                Password = section.Require("Password"),
+                Uri = billingSection.Require("ServerURL"),
+                ResourceName = billingSection.Require("InitialCatalog"),
+                UserName = billingSection.Require("UserName"),
+                Password = billingSection.Require("Password"),
             };
 
-            services.AddSingleton<LagoVista.CloudStorage.ICacheProviderSettings, CacheProviderSettings>();
-            services.AddScoped<IKeyIdTargetResolver, Services.KeyIdTargetResolver>();   
+            var semanticSection = configurationRoot.GetSection("SemanticDb");
+            var semanticConnectionSettings = new ConnectionSettings()
+            {
+                Uri = semanticSection.Require("ServerURL"),
+                ResourceName = semanticSection.Require("InitialCatalog"),
+                UserName = semanticSection.Require("UserName"),
+                Password = semanticSection.Require("Password"),
+            };
 
-            var connectionString = $"Server=tcp:{connectionSettings.Uri},1433;Initial Catalog={connectionSettings.ResourceName};Persist Security Info=False;User ID={connectionSettings.UserName};Password={connectionSettings.Password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            var semanticTestSection = configurationRoot.GetSection("SemanticTestDb");
+            var semanticTestConnectionSettings = new ConnectionSettings()
+            {
+                Uri = semanticTestSection.Require("ServerURL"),
+                ResourceName = semanticTestSection.Require("InitialCatalog"),
+                UserName = semanticTestSection.Require("UserName"),
+                Password = semanticTestSection.Require("Password"),
+            };
 
-            services.AddDbContext<BillingDataContext>(
-                options => options.UseSqlServer(connectionString, moreOptions => {
-                    moreOptions.EnableRetryOnFailure();
-                    }),
-                  
-                contextLifetime: ServiceLifetime.Scoped,
-                optionsLifetime: ServiceLifetime.Singleton);
+            services.AddSingleton<ICacheProviderSettings, CacheProviderSettings>();
+            services.AddScoped<IKeyIdTargetResolver, Services.KeyIdTargetResolver>();
 
-            services.AddDbContextFactory<BillingDataContext>(options => {
-                options.UseSqlServer(connectionString, moreOptions => moreOptions.EnableRetryOnFailure());
+            var billingConnectionString = BuildSqlConnectionString(billingConnectionSettings);
+            var semanticConnectionString = BuildSqlConnectionString(semanticConnectionSettings);
+            var semanticTestConnectionString = BuildSqlConnectionString(semanticTestConnectionSettings);
+
+            services.AddDbContext<BillingDataContext>(options => options.UseSqlServer(billingConnectionString, moreOptions => moreOptions.EnableRetryOnFailure()), contextLifetime: ServiceLifetime.Scoped, optionsLifetime: ServiceLifetime.Singleton);
+            services.AddDbContextFactory<BillingDataContext>(options =>
+            {
+                options.UseSqlServer(billingConnectionString, moreOptions => moreOptions.EnableRetryOnFailure());
                 options.ConfigureWarnings(warnings => warnings.Ignore(RelationalEventId.CommandExecuted));
+                options.LogTo(msg => { }, Microsoft.Extensions.Logging.LogLevel.Information);
+            });
 
-                options.LogTo(msg =>
-                {
-                    
-                }, Microsoft.Extensions.Logging.LogLevel.Information);
-                }); 
+            services.AddDbContext<SemanticDataContext>(options => options.UseSqlServer(semanticConnectionString, moreOptions => moreOptions.EnableRetryOnFailure()), contextLifetime: ServiceLifetime.Scoped, optionsLifetime: ServiceLifetime.Singleton);
+            services.AddDbContextFactory<SemanticDataContext>(options =>
+            {
+                options.UseSqlServer(semanticConnectionString, moreOptions => moreOptions.EnableRetryOnFailure());
+                options.ConfigureWarnings(warnings => warnings.Ignore(RelationalEventId.CommandExecuted));
+                options.LogTo(msg => { }, Microsoft.Extensions.Logging.LogLevel.Information);
+            });
+
+            services.AddDbContext<SemanticTestDataContext>(options => options.UseSqlServer(semanticTestConnectionString, moreOptions => moreOptions.EnableRetryOnFailure()), contextLifetime: ServiceLifetime.Scoped, optionsLifetime: ServiceLifetime.Singleton);
+            services.AddDbContextFactory<SemanticTestDataContext>(options =>
+            {
+                options.UseSqlServer(semanticTestConnectionString, moreOptions => moreOptions.EnableRetryOnFailure());
+                options.ConfigureWarnings(warnings => warnings.Ignore(RelationalEventId.CommandExecuted));
+                options.LogTo(msg => { }, Microsoft.Extensions.Logging.LogLevel.Information);
+            });
         }
+
+        private static string BuildSqlConnectionString(ConnectionSettings connectionSettings) => $"Server=tcp:{connectionSettings.Uri},1433;Initial Catalog={connectionSettings.ResourceName};Persist Security Info=False;User ID={connectionSettings.UserName};Password={connectionSettings.Password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
     }
 }
 
@@ -62,6 +86,6 @@ namespace LagoVista.DependencyInjection
         public static void AddRelationalStorageModule(this IServiceCollection services, IConfigurationRoot configurationRoot, ILogger logger)
         {
             LagoVista.Relational.Startup.ConfigureDataContextServices(configurationRoot, services, logger);
-        }   
+        }
     }
 }
