79e1591d7b721342d5ea130bb86d30953992d0bb
Kevin D. Wolf <kevinw@software-logistics.com>
2026-08-19 13:29:59 -0400

    Load required cache password

--- src/LagoVista.Relational/CacheProviderSettings.cs
+++ src/LagoVista.Relational/CacheProviderSettings.cs
@@ -11,11 +11,14 @@ namespace LagoVista.CloudStorage
 
         public IConnectionSettings CacheSettings { get; }
 
+        public string Password { get; }
+
         public CacheProviderSettings(IConfiguration configuration)
         {
             var cacheSection = configuration.GetSection("SystemCache");
 
             UseCache = Convert.ToBoolean(cacheSection.Require("UseCache"));
+            Password = cacheSection.Require("Password");
             CacheSettings = new ConnectionSettings
             {
                 Uri = cacheSection.Require("Uri"),
