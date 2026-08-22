dcd574d63d1ff42c8f7016933ec2b26b78bd02b3
Kevin D. Wolf <kevinw@software-logistics.com>
2026-08-22 18:51:40 -0400

    Add typed projection queries to document collection

--- src/LagoVista.CloudStorage/Interfaces/IDocumentCollection.cs
+++ src/LagoVista.CloudStorage/Interfaces/IDocumentCollection.cs
@@ -9,6 +9,7 @@ namespace LagoVista.CloudStorage.Interfaces
     public interface IDocumentCollection
     {
         Task<ListResponse<TDocument>> QueryAsync<TDocument>(Expression<Func<TDocument, bool>> query, Expression<Func<TDocument, string>> sort, ListRequest listRequest, CancellationToken cancellationToken = default) where TDocument : class;
+        Task<ListResponse<TProjection>> QueryAsync<TDocument, TProjection, TSort>(Expression<Func<TDocument, bool>> query, Expression<Func<TDocument, TProjection>> projection, Expression<Func<TDocument, TSort>> sort, ListRequest listRequest, CancellationToken cancellationToken = default) where TDocument : class where TProjection : class;
     }
 
     public interface IDocumentCollectionFactory
