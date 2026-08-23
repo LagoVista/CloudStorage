fd9ed4a3a0939e0f8127d4c3af6e72b50ff155bc
Kevin D. Wolf <kevinw@software-logistics.com>
2026-08-23 11:53:23 -0400

    Rename application data store contract tests

--- tests/LagoVista.CloudStorage.Tests/StorageContractTests.cs
+++ tests/LagoVista.CloudStorage.Tests/StorageContractTests.cs
@@ -22,51 +22,40 @@ namespace LagoVista.CloudStorage.Tests
         private class FakeAppendStore : IAppendHistoryStore<TestEntity>
         {
             public FakeAppendStore() { }
-
             public Task InsertAsync(TestEntity entity, CancellationToken cancellationToken = default) => Task.CompletedTask;
             public Task InsertBatchAsync(IEnumerable<TestEntity> entities, CancellationToken cancellationToken = default) => Task.CompletedTask;
-            public Task<StoragePageResult<TestEntity>> QueryAsync(HistoryQuery<TestEntity> query, CancellationToken cancellationToken = default) =>
-                Task.FromResult(new StoragePageResult<TestEntity>(Array.Empty<TestEntity>()));
+            public Task<StoragePageResult<TestEntity>> QueryAsync(HistoryQuery<TestEntity> query, CancellationToken cancellationToken = default) => Task.FromResult(new StoragePageResult<TestEntity>(Array.Empty<TestEntity>()));
         }
 
         private class FakeScratchStore : IScratchStore<TestEntity>
         {
             public FakeScratchStore() { }
-
             public Task<TestEntity> GetAsync(StorageKey key, CancellationToken cancellationToken = default) => Task.FromResult<TestEntity>(null);
             public Task UpsertAsync(TestEntity entity, CancellationToken cancellationToken = default) => Task.CompletedTask;
             public Task DeleteAsync(StorageKey key, CancellationToken cancellationToken = default) => Task.CompletedTask;
-            public Task<StoragePageResult<TestEntity>> QueryAsync(StorageQuery<TestEntity> query, CancellationToken cancellationToken = default) =>
-                Task.FromResult(new StoragePageResult<TestEntity>(Array.Empty<TestEntity>()));
+            public Task<StoragePageResult<TestEntity>> QueryAsync(StorageQuery<TestEntity> query, CancellationToken cancellationToken = default) => Task.FromResult(new StoragePageResult<TestEntity>(Array.Empty<TestEntity>()));
         }
 
-        private class FakeFlatDocumentStore : IFlatDocumentStore<TestEntity>
+        private class FakeApplicationDataStore : IApplicationDataStore<TestEntity>
         {
-            public FakeFlatDocumentStore() { }
-
+            public FakeApplicationDataStore() { }
             public Task<TestEntity> GetAsync(StorageKey key, CancellationToken cancellationToken = default) => Task.FromResult<TestEntity>(null);
             public Task InsertAsync(TestEntity entity, CancellationToken cancellationToken = default) => Task.CompletedTask;
             public Task UpdateAsync(TestEntity entity, CancellationToken cancellationToken = default) => Task.CompletedTask;
             public Task DeleteAsync(StorageKey key, CancellationToken cancellationToken = default) => Task.CompletedTask;
-            public Task<StoragePageResult<TestEntity>> QueryAsync(StorageQuery<TestEntity> query, CancellationToken cancellationToken = default) =>
-                Task.FromResult(new StoragePageResult<TestEntity>(Array.Empty<TestEntity>()));
+            public Task<StoragePageResult<TestEntity>> QueryAsync(StorageQuery<TestEntity> query, CancellationToken cancellationToken = default) => Task.FromResult(new StoragePageResult<TestEntity>(Array.Empty<TestEntity>()));
         }
 
         [Test]
         public void AddAppendHistoryStore_ResolvesCapabilityAndTypedOptions()
         {
             var services = new ServiceCollection();
-            services.AddAppendHistoryStore<TestEntity, FakeAppendStore>(storage => storage
-                .PartitionBy(x => x.OrganizationId)
-                .TimeBy(x => x.Timestamp)
-                .BucketBy(StoragePeriod.Month)
-                .Index(x => x.Category));
+            services.AddAppendHistoryStore<TestEntity, FakeAppendStore>(storage => storage.PartitionBy(x => x.OrganizationId).TimeBy(x => x.Timestamp).BucketBy(StoragePeriod.Month).Index(x => x.Category));
 
             using (var provider = services.BuildServiceProvider())
             using (var scope = provider.CreateScope())
             {
                 Assert.That(scope.ServiceProvider.GetRequiredService<IAppendHistoryStore<TestEntity>>(), Is.TypeOf<FakeAppendStore>());
-
                 var options = scope.ServiceProvider.GetRequiredService<AppendHistoryStoreOptions<TestEntity>>();
                 Assert.That(options.Definition.TimeField, Is.EqualTo(nameof(TestEntity.Timestamp)));
                 Assert.That(options.Definition.PartitionFields, Does.Contain(nameof(TestEntity.OrganizationId)));
@@ -79,28 +68,22 @@ namespace LagoVista.CloudStorage.Tests
         public void AddAppendHistoryStore_WithoutTimeField_FailsFast()
         {
             var services = new ServiceCollection();
-
-            var ex = Assert.Throws<InvalidOperationException>(() =>
-                services.AddAppendHistoryStore<TestEntity, FakeAppendStore>(storage =>
-                    storage.PartitionBy(x => x.OrganizationId)));
-
+            var ex = Assert.Throws<InvalidOperationException>(() => services.AddAppendHistoryStore<TestEntity, FakeAppendStore>(storage => storage.PartitionBy(x => x.OrganizationId)));
             Assert.That(ex.Message, Does.Contain("TimeBy"));
         }
 
         [Test]
-        public void ScratchAndFlatDocument_RemainSeparateCapabilities()
+        public void ScratchAndApplicationData_RemainSeparateCapabilities()
         {
             var services = new ServiceCollection();
             services.AddScratchStore<TestEntity, FakeScratchStore>(storage => storage.KeyBy(x => x.Id));
-            services.AddFlatDocumentStore<TestEntity, FakeFlatDocumentStore>(storage => storage
-                .KeyBy(x => x.Id)
-                .Index(x => x.Category));
+            services.AddApplicationDataStore<TestEntity, FakeApplicationDataStore>(storage => storage.KeyBy(x => x.Id).Index(x => x.Category));
 
             using (var provider = services.BuildServiceProvider())
             using (var scope = provider.CreateScope())
             {
                 Assert.That(scope.ServiceProvider.GetRequiredService<IScratchStore<TestEntity>>(), Is.TypeOf<FakeScratchStore>());
-                Assert.That(scope.ServiceProvider.GetRequiredService<IFlatDocumentStore<TestEntity>>(), Is.TypeOf<FakeFlatDocumentStore>());
+                Assert.That(scope.ServiceProvider.GetRequiredService<IApplicationDataStore<TestEntity>>(), Is.TypeOf<FakeApplicationDataStore>());
             }
         }
 
@@ -108,22 +91,16 @@ namespace LagoVista.CloudStorage.Tests
         public void MutableStores_WithoutKeyField_FailFast()
         {
             var scratchServices = new ServiceCollection();
-            var flatServices = new ServiceCollection();
+            var applicationServices = new ServiceCollection();
 
-            Assert.Throws<InvalidOperationException>(() =>
-                scratchServices.AddScratchStore<TestEntity, FakeScratchStore>(storage => storage.Index(x => x.Category)));
-
-            Assert.Throws<InvalidOperationException>(() =>
-                flatServices.AddFlatDocumentStore<TestEntity, FakeFlatDocumentStore>(storage => storage.Index(x => x.Category)));
+            Assert.Throws<InvalidOperationException>(() => scratchServices.AddScratchStore<TestEntity, FakeScratchStore>(storage => storage.Index(x => x.Category)));
+            Assert.Throws<InvalidOperationException>(() => applicationServices.AddApplicationDataStore<TestEntity, FakeApplicationDataStore>(storage => storage.Index(x => x.Category)));
         }
 
         [Test]
         public void QueryModels_UseTypedSelectorsAndOpaquePaging()
         {
-            var query = new StorageQuery<TestEntity>()
-                .Where(x => x.Category, StorageFilterOperator.Equal, "telemetry")
-                .OrderBy(x => x.Timestamp, StorageSortDirection.Descending)
-                .WithPage(new StoragePageRequest(250, "opaque-token"));
+            var query = new StorageQuery<TestEntity>().Where(x => x.Category, StorageFilterOperator.Equal, "telemetry").OrderBy(x => x.Timestamp, StorageSortDirection.Descending).WithPage(new StoragePageRequest(250, "opaque-token"));
 
             Assert.That(query.Filters.Single().Field, Is.EqualTo(nameof(TestEntity.Category)));
             Assert.That(query.Sorts.Single().Field, Is.EqualTo(nameof(TestEntity.Timestamp)));
@@ -135,7 +112,6 @@ namespace LagoVista.CloudStorage.Tests
         public void AppendHistoryContract_DoesNotExposeMutationOperations()
         {
             var methodNames = typeof(IAppendHistoryStore<TestEntity>).GetMethods().Select(method => method.Name).ToList();
-
             Assert.That(methodNames, Does.Not.Contain("UpdateAsync"));
             Assert.That(methodNames, Does.Not.Contain("DeleteAsync"));
             Assert.That(methodNames, Does.Contain("InsertAsync"));
