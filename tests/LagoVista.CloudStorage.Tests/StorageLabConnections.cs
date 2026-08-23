14e944abedc542786182eece3313f66694b596dc
Kevin D. Wolf <kevinw@software-logistics.com>
2026-08-23 16:10:04 -0400

    Add deterministic storage lab connections

---
+++ tests/LagoVista.CloudStorage.Tests/StorageLabConnections.cs
@@ -0,0 +1,18 @@
+using LagoVista.Core.Models;
+
+namespace LagoVista.CloudStorage.Tests
+{
+    internal static class StorageLabConnections
+    {
+        public const string CosmosEndpoint = "https://localhost:8081/";
+        public const string CosmosKey = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+        public const string CosmosDatabase = "CloudStorageLab";
+
+        public static ConnectionSettings TestCosmosDocumentStorage => new ConnectionSettings
+        {
+            Uri = CosmosEndpoint,
+            AccessKey = CosmosKey,
+            ResourceName = CosmosDatabase
+        };
+    }
+}
