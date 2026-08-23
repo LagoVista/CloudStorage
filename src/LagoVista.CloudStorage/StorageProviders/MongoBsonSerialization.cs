d8f0cc2bdd226c99ee3c6796ecbefb466091b6d0
Kevin D. Wolf <kevinw@software-logistics.com>
2026-08-23 14:49:07 -0400

    Add Mongo UtcTimestamp BSON serialization

---
+++ src/LagoVista.CloudStorage/StorageProviders/MongoBsonSerialization.cs
@@ -0,0 +1,53 @@
+using LagoVista.Core;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using System;
+
+namespace LagoVista.CloudStorage.StorageProviders
+{
+    internal static class MongoBsonSerialization
+    {
+        private static readonly object _syncRoot = new object();
+        private static bool _configured;
+
+        public static void Configure()
+        {
+            if (_configured) return;
+
+            lock (_syncRoot)
+            {
+                if (_configured) return;
+                BsonSerializer.RegisterSerializer(typeof(UtcTimestamp), new UtcTimestampBsonSerializer());
+                _configured = true;
+            }
+        }
+    }
+
+    internal sealed class UtcTimestampBsonSerializer : SerializerBase<UtcTimestamp>
+    {
+        public override UtcTimestamp Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
+        {
+            if (context == null) throw new ArgumentNullException(nameof(context));
+
+            var reader = context.Reader;
+            switch (reader.GetCurrentBsonType())
+            {
+                case BsonType.String:
+                    return UtcTimestamp.Parse(reader.ReadString());
+
+                case BsonType.DateTime:
+                    return UtcTimestamp.FromDateTime(BsonUtils.ToDateTimeFromMillisecondsSinceEpoch(reader.ReadDateTime()));
+
+                default:
+                    throw new BsonSerializationException($"Cannot deserialize {nameof(UtcTimestamp)} from BSON type {reader.GetCurrentBsonType()}.");
+            }
+        }
+
+        public override void Serialize(BsonSerializationContext context, BsonSerializationArgs args, UtcTimestamp value)
+        {
+            if (context == null) throw new ArgumentNullException(nameof(context));
+            context.Writer.WriteString(value.ToString());
+        }
+    }
+}
