010c20ace4f11cbccb037f7294a73f6788cd92cb
Kevin D. Wolf <kevinw@software-logistics.com>
2026-08-23 16:35:25 -0400

    Use alternate Cosmos lab endpoint

--- tests/LagoVista.CloudStorage.Tests/StorageLabConnections.cs
+++ tests/LagoVista.CloudStorage.Tests/StorageLabConnections.cs
@@ -4,7 +4,7 @@ namespace LagoVista.CloudStorage.Tests
 {
     internal static class StorageLabConnections
     {
-        public const string CosmosEndpoint = "https://localhost:8081/";
+        public const string CosmosEndpoint = "https://localhost:18081/";
         public const string CosmosKey = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
         public const string CosmosDatabase = "CloudStorageLab";
 
