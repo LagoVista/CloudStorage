fec3faaeeb2b44de2334b33877964df962ce53f1
Kevin D. Wolf <kevinw@software-logistics.com>
2026-08-23 14:57:10 -0400

    Use fixed Cassandra Docker test connection

--- tests/LagoVista.CloudStorage.Tests/run-cassandra-tests.ps1
+++ tests/LagoVista.CloudStorage.Tests/run-cassandra-tests.ps1
@@ -3,13 +3,6 @@ $ErrorActionPreference = "Stop"
 & (Join-Path $PSScriptRoot "start-cassandra-tests.ps1")
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
-$env:TEST_CASSANDRA_CONTACT_POINTS = "localhost"
-$env:TEST_CASSANDRA_PORT = "19042"
-$env:TEST_CASSANDRA_USERNAME = "cassandra"
-$env:TEST_CASSANDRA_PASSWORD = "cassandra"
-$env:TEST_CASSANDRA_KEYSPACE = "nuviot_cloudstorage_tests"
-$env:TEST_CASSANDRA_LOCAL_DATACENTER = "datacenter1"
-
 $project = Join-Path $PSScriptRoot "LagoVista.CloudStorage.IntegrationTests.csproj"
 Write-Host "Running Cassandra integration tests against local Docker Cassandra..."
 dotnet test $project --filter "TestCategory=Cassandra"
