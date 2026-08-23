0e173d9072dc20252365c92f7d118f584ebc0415
Kevin D. Wolf <kevinw@software-logistics.com>
2026-08-23 09:09:57 -0400

    Test resolved rich document storage factory

--- tests/LagoVista.CloudStorage.Tests/RichMongoProviderTests.cs
+++ tests/LagoVista.CloudStorage.Tests/RichMongoProviderTests.cs
@@ -2,9 +2,11 @@ using LagoVista.CloudStorage.DocumentDB;
 using LagoVista.Core.Attributes;
 using LagoVista.Core.Models;
 using NUnit.Framework;
+using System;
 
 namespace LagoVista.CloudStorage.Tests
 {
+    [NonParallelizable]
     public class RichMongoProviderTests
     {
         [Test]
@@ -28,11 +30,52 @@ namespace LagoVista.CloudStorage.Tests
         }
 
         [Test]
+        public void DocumentStorageFactory_ResolveAndCreate_WithDatabaseMongoOverride_CreatesMongoProvider()
+        {
+            var providerVariable = DocumentStorageSettingsResolver.ProviderEnvironmentVariablePrefix + "LOGICALDB";
+            var connectionVariable = DocumentStorageSettingsResolver.MongoConnectionStringEnvironmentVariablePrefix + "LOGICALDB";
+            var databaseVariable = DocumentStorageSettingsResolver.MongoDatabaseEnvironmentVariablePrefix + "LOGICALDB";
+
+            WithEnvironment(providerVariable, "mongo", () => WithEnvironment(connectionVariable, "mongodb://localhost:27017", () => WithEnvironment(databaseVariable, "MongoTarget", () =>
+            {
+                var storage = DocumentStorageFactory.ResolveAndCreate<RichMongoTestEntity>("https://cosmos.example:443/", "cosmos-key", "LogicalDb", null);
+                Assert.That(storage.GetCollectionName(), Is.EqualTo("RichMongoDomain"));
+                Assert.That(storage.GetPartitionKey(), Is.Null);
+            })));
+        }
+
+        [Test]
+        public void DocumentStorageFactory_ResolveAndCreate_WithNoProviderSetting_CreatesCosmosProvider()
+        {
+            var providerVariable = DocumentStorageSettingsResolver.ProviderEnvironmentVariablePrefix + "LOGICALDB";
+            WithEnvironment(providerVariable, null, () => WithEnvironment(DocumentStorageSettingsResolver.ProviderEnvironmentVariable, null, () =>
+            {
+                var storage = DocumentStorageFactory.ResolveAndCreate<RichMongoTestEntity>("https://cosmos.example:443/", "cosmos-key", "LogicalDb", null);
+                Assert.That(storage.GetCollectionName(), Is.EqualTo("LogicalDb_Collections"));
+                Assert.That(storage.GetPartitionKey(), Is.EqualTo("/_partitionKey"));
+            }));
+        }
+
+        [Test]
         public void OperationResponse_WithProviderNeutralResource_ReturnsResource()
         {
             var entity = new RichMongoTestEntity { Id = "ABC123", Name = "Test" };
             var response = new OperationResponse<RichMongoTestEntity>(entity);
             Assert.That(response.Resource, Is.SameAs(entity));
+        }
+
+        private static void WithEnvironment(string variableName, string value, Action action)
+        {
+            var priorValue = Environment.GetEnvironmentVariable(variableName);
+            try
+            {
+                Environment.SetEnvironmentVariable(variableName, value);
+                action();
+            }
+            finally
+            {
+                Environment.SetEnvironmentVariable(variableName, priorValue);
+            }
         }
 
         [EntityDescription("RichMongoDomain", "", "", "", EntityDescriptionAttribute.EntityTypes.Dto, typeof(RichMongoProviderTests))]
