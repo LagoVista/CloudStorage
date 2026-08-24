d3bd5179fdf42bfc2ef369ea140afba67f5279df
Kevin D. Wolf <kevinw@software-logistics.com>
2026-08-24 16:07:35 -0400

    Test three-way entity storage routing

--- tests/LagoVista.CloudStorage.Tests/DocumentCollectionNameResolverTests.cs
+++ tests/LagoVista.CloudStorage.Tests/DocumentCollectionNameResolverTests.cs
@@ -1,78 +1,124 @@
 using LagoVista.CloudStorage.DocumentDB;
 using LagoVista.Core.Attributes;
 using NUnit.Framework;
+using System;
 
 namespace LagoVista.CloudStorage.Tests
 {
     public class DocumentCollectionNameResolverTests
     {
         [Test]
-        public void Resolve_WithTwoTypesInSameDomain_ReturnsSameCollection()
+        public void Resolve_WithShareableStorage_ReturnsSharedEntitiesCollection()
         {
             var resolver = new DocumentCollectionNameResolver();
-            var first = resolver.Resolve("TestDb", typeof(FirstDomainEntity));
-            var second = resolver.Resolve("TestDb", typeof(SecondDomainEntity));
+            var collection = resolver.Resolve("TestDb", typeof(ShareableEntity));
 
-            Assert.That(first, Is.EqualTo("SharedDomain"));
-            Assert.That(second, Is.EqualTo("SharedDomain"));
+            Assert.That(collection, Is.EqualTo(DocumentCollectionNameResolver.SharedEntitiesCollectionName));
+        }
+
+        [Test]
+        public void Resolve_WithNoStorageAttribute_ReturnsOrganizationEntitiesCollection()
+        {
+            var resolver = new DocumentCollectionNameResolver();
+            var collection = resolver.Resolve("TestDb", typeof(OrganizationEntity));
+
+            Assert.That(collection, Is.EqualTo(DocumentCollectionNameResolver.OrganizationEntitiesCollectionName));
+        }
+
+        [Test]
+        public void Resolve_WithDedicatedStorageCollection_ReturnsEntityTypeName()
+        {
+            var resolver = new DocumentCollectionNameResolver();
+            var collection = resolver.Resolve("TestDb", typeof(DedicatedEntity));
+
+            Assert.That(collection, Is.EqualTo(nameof(DedicatedEntity)));
         }
 
         [Test]
         public void Resolve_WithExplicitCollection_UsesExplicitCollection()
         {
             var resolver = new DocumentCollectionNameResolver();
-            var collection = resolver.Resolve("TestDb", typeof(FirstDomainEntity), "SpecialCollection");
+            var collection = resolver.Resolve("TestDb", typeof(OrganizationEntity), "SpecialCollection");
+
             Assert.That(collection, Is.EqualTo("SpecialCollection"));
         }
 
         [Test]
-        public void Resolve_WithMissingEntityDescription_UsesFallback()
+        public void Resolve_WithConflictingStorageAttributes_Throws()
         {
             var resolver = new DocumentCollectionNameResolver();
-            var collection = resolver.Resolve("TestDb", typeof(UnattributedEntity));
-            Assert.That(collection, Is.EqualTo("TestDb_Collections"));
+
+            var exception = Assert.Throws<InvalidOperationException>(() => resolver.Resolve("TestDb", typeof(InvalidStorageEntity)));
+            Assert.That(exception.Message, Does.Contain(nameof(InvalidStorageEntity)));
         }
 
         [Test]
-        public void TryResolve_WithLoadedEntityType_UsesDomain()
+        public void TryResolve_WithLoadedShareableEntity_UsesSharedEntitiesCollection()
         {
             var resolver = new DocumentCollectionNameResolver();
-            var resolved = resolver.TryResolve("TestDb", nameof(MigrationLookupEntity), out var collection);
+            var resolved = resolver.TryResolve("TestDb", nameof(ShareableMigrationEntity), out var collection);
 
             Assert.That(resolved, Is.True);
-            Assert.That(collection, Is.EqualTo("MigrationDomain"));
+            Assert.That(collection, Is.EqualTo(DocumentCollectionNameResolver.SharedEntitiesCollectionName));
         }
 
         [Test]
-        public void TryResolve_WithUnknownEntityType_ReturnsVisibleFallback()
+        public void TryResolve_WithLoadedOrganizationEntity_UsesOrganizationEntitiesCollection()
+        {
+            var resolver = new DocumentCollectionNameResolver();
+            var resolved = resolver.TryResolve("TestDb", nameof(OrganizationMigrationEntity), out var collection);
+
+            Assert.That(resolved, Is.True);
+            Assert.That(collection, Is.EqualTo(DocumentCollectionNameResolver.OrganizationEntitiesCollectionName));
+        }
+
+        [Test]
+        public void TryResolve_WithLoadedDedicatedEntity_UsesEntityTypeName()
+        {
+            var resolver = new DocumentCollectionNameResolver();
+            var resolved = resolver.TryResolve("TestDb", nameof(DedicatedMigrationEntity), out var collection);
+
+            Assert.That(resolved, Is.True);
+            Assert.That(collection, Is.EqualTo(nameof(DedicatedMigrationEntity)));
+        }
+
+        [Test]
+        public void TryResolve_WithUnknownEntityType_ReturnsOrganizationFallback()
         {
             var resolver = new DocumentCollectionNameResolver();
             var resolved = resolver.TryResolve("TestDb", "DefinitelyNotARealEntityType", out var collection);
 
             Assert.That(resolved, Is.False);
-            Assert.That(collection, Is.EqualTo("TestDb_Collections"));
+            Assert.That(collection, Is.EqualTo(DocumentCollectionNameResolver.OrganizationEntitiesCollectionName));
         }
 
         [Test]
-        public void Resolve_WithMongoReservedCharacters_NormalizesCollectionName()
+        public void Resolve_WithExplicitMongoReservedCharacters_NormalizesCollectionName()
         {
             var resolver = new DocumentCollectionNameResolver();
-            var collection = resolver.Resolve("TestDb", typeof(ReservedCharacterDomainEntity));
+            var collection = resolver.Resolve("TestDb", typeof(OrganizationEntity), "Domain$Name");
+
             Assert.That(collection, Is.EqualTo("Domain_Name"));
         }
 
-        [EntityDescription("SharedDomain", "", "", "", EntityDescriptionAttribute.EntityTypes.Dto, typeof(DocumentCollectionNameResolverTests))]
-        private sealed class FirstDomainEntity { }
+        [ShareableStorage]
+        private sealed class ShareableEntity { }
 
-        [EntityDescription("SharedDomain", "", "", "", EntityDescriptionAttribute.EntityTypes.Dto, typeof(DocumentCollectionNameResolverTests))]
-        private sealed class SecondDomainEntity { }
+        private sealed class OrganizationEntity { }
 
-        [EntityDescription("MigrationDomain", "", "", "", EntityDescriptionAttribute.EntityTypes.Dto, typeof(DocumentCollectionNameResolverTests))]
-        private sealed class MigrationLookupEntity { }
+        [DedicatedStorageCollection]
+        private sealed class DedicatedEntity { }
 
-        [EntityDescription("Domain$Name", "", "", "", EntityDescriptionAttribute.EntityTypes.Dto, typeof(DocumentCollectionNameResolverTests))]
-        private sealed class ReservedCharacterDomainEntity { }
+        [ShareableStorage]
+        [DedicatedStorageCollection]
+        private sealed class InvalidStorageEntity { }
 
-        private sealed class UnattributedEntity { }
+        [ShareableStorage]
+        private sealed class ShareableMigrationEntity { }
+
+        private sealed class OrganizationMigrationEntity { }
+
+        [DedicatedStorageCollection]
+        private sealed class DedicatedMigrationEntity { }
     }
 }
