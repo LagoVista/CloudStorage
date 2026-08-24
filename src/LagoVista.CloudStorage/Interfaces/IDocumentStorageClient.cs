a5bc26021b10e752104e764121764bd9abc3a5fa
Kevin D. Wolf <kevinw@software-logistics.com>
2026-08-24 11:05:20 -0400

    Add document storage client contracts

---
+++ src/LagoVista.CloudStorage/Interfaces/IDocumentStorageClient.cs
@@ -0,0 +1,54 @@
+using LagoVista.CloudStorage.DocumentDB;
+using LagoVista.Core.Interfaces;
+using LagoVista.Core.Models.UIMetaData;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LagoVista.CloudStorage.Interfaces
+{
+    public interface IDocumentStorageClient
+    {
+        Task<OperationResponse<TEntity>> CreateDocumentAsync<TEntity>(TEntity item)
+            where TEntity : class, IIDEntity, IKeyedEntity, IOwnedEntity, INamedEntity, INoSQLEntity, IAuditableEntity;
+
+        Task<OperationResponse<TEntity>> UpsertDocumentAsync<TEntity>(TEntity item)
+            where TEntity : class, IIDEntity, IKeyedEntity, IOwnedEntity, INamedEntity, INoSQLEntity, IAuditableEntity;
+
+        Task<TEntity> GetDocumentAsync<TEntity>(string id, bool throwOnNotFound = true)
+            where TEntity : class, IIDEntity, IKeyedEntity, IOwnedEntity, INamedEntity, INoSQLEntity, IAuditableEntity;
+
+        Task<TEntity> GetDocumentAsync<TEntity>(string id, string partitionKey, bool throwOnNotFound = true)
+            where TEntity : class, IIDEntity, IKeyedEntity, IOwnedEntity, INamedEntity, INoSQLEntity, IAuditableEntity;
+
+        Task<OperationResponse<TEntity>> DeleteDocumentAsync<TEntity>(string id)
+            where TEntity : class, IIDEntity, IKeyedEntity, IOwnedEntity, INamedEntity, INoSQLEntity, IAuditableEntity;
+
+        Task<OperationResponse<TEntity>> DeleteDocumentAsync<TEntity>(string id, string partitionKey)
+            where TEntity : class, IIDEntity, IKeyedEntity, IOwnedEntity, INamedEntity, INoSQLEntity, IAuditableEntity;
+
+        Task<IEnumerable<TEntity>> QueryAsync<TEntity>(Expression<Func<TEntity, bool>> query)
+            where TEntity : class, IIDEntity, IKeyedEntity, IOwnedEntity, INamedEntity, INoSQLEntity, IAuditableEntity;
+
+        Task<ListResponse<TEntity>> QueryAsync<TEntity>(Expression<Func<TEntity, bool>> query, ListRequest listRequest)
+            where TEntity : class, IIDEntity, IKeyedEntity, IOwnedEntity, INamedEntity, INoSQLEntity, IAuditableEntity;
+
+        Task<IEnumerable<TResult>> QueryKnownAsync<TResult>(string entityType, DocumentQueryRequest request, CancellationToken cancellationToken = default)
+            where TResult : class;
+    }
+
+    public interface ICosmosDocumentStorageClient : IDocumentStorageClient
+    {
+    }
+
+    public interface IMongoDocumentStorageClient : IDocumentStorageClient
+    {
+    }
+
+    public interface IDocumentStorageClientProvider
+    {
+        IDocumentStorageClient GetClient();
+    }
+}
