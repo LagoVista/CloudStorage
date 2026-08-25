07c8637650f4476655d9cf1296ba23b007092369
Kevin D. Wolf <kevinw@software-logistics.com>
2026-08-25 15:12:40 -0400

    Align current repository namespaces

--- src/LagoVista.CloudStorage/Repositories/EntityDetailResponseFactory.cs
+++ src/LagoVista.CloudStorage/Repositories/EntityDetailResponseFactory.cs
@@ -10,7 +10,7 @@ using System;
 using System.Reflection;
 using System.Threading.Tasks;
 
-namespace LagoVista.CloudStorage.Storage
+namespace LagoVista.CloudStorage.Repositories
 {
     public class EntityDetailResponseFactory : IEntityDetailResponseFactory
     {
