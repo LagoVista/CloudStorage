e382ba61cea3c502cc1b5ea0daef7ffb31969f7d
Kevin D. Wolf <kevinw@software-logistics.com>
2026-08-26 16:21:20 -0400

    Mark Mongo document storage client as critical coverage

--- src/LagoVista.CloudStorage/Storage/StorageProviders/Mongo/MongoDocumentStorageClient.DatabaseName.cs
+++ src/LagoVista.CloudStorage/Storage/StorageProviders/Mongo/MongoDocumentStorageClient.DatabaseName.cs
@@ -1,5 +1,8 @@
+using LagoVista.Core;
+
 namespace LagoVista.CloudStorage.StorageProviders
 {
+    [CriticalCoverage]
     public sealed partial class MongoDocumentStorageClient
     {
         public string DatabaseName => _settings.DatabaseName;
