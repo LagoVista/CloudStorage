dde1ad632b0f1e06f2183726254f78f5683cd3c9
Kevin D. Wolf <kevinw@software-logistics.com>
2026-08-26 09:32:20 -0400

    Add Cosmos owned document lookup

---
+++ src/LagoVista.CloudStorage/Storage/StorageProviders/CosmosDB/CosmosDocumentStorageClient.OwnedLookup.cs
@@ -0,0 +1,32 @@
+using LagoVista.CloudStorage.DocumentDB;
+using LagoVista.Core.Exceptions;
+using Microsoft.Azure.Cosmos;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LagoVista.CloudStorage.StorageProviders
+{
+    public sealed partial class CosmosDocumentStorageClient
+    {
+        public async Task<TProjection> GetOwnedDocumentProjectionAsync<TProjection>(string id, string ownerOrganizationId, bool throwOnNotFound = true, CancellationToken cancellationToken = default) where TProjection : class
+        {
+            if (String.IsNullOrWhiteSpace(id)) throw new ArgumentException("Document id is required.", nameof(id));
+            if (String.IsNullOrWhiteSpace(ownerOrganizationId)) throw new ArgumentException("Owner organization id is required.", nameof(ownerOrganizationId));
+
+            var collectionResolver = new DocumentCollectionNameResolver();
+            var collectionName = collectionResolver.GetFallback(_settings.DatabaseName);
+            var container = _cosmosClientProvider.GetClient(_settings.Endpoint, _settings.AccessKey).GetContainer(_settings.DatabaseName, collectionName);
+            var query = new QueryDefinition("SELECT TOP 1 * FROM c WHERE c.id = @id AND c.OwnerOrganization.Id = @ownerOrganizationId").WithParameter("@id", id.Trim()).WithParameter("@ownerOrganizationId", ownerOrganizationId);
+            using var iterator = container.GetItemQueryIterator<TProjection>(query);
+            if (iterator.HasMoreResults)
+            {
+                var response = await iterator.ReadNextAsync(cancellationToken).ConfigureAwait(false);
+                foreach (var item in response) return item;
+            }
+
+            if (throwOnNotFound) throw new RecordNotFoundException(typeof(TProjection).Name, id);
+            return null;
+        }
+    }
+}
