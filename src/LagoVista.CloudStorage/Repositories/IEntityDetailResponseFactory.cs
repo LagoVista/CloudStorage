3e07db894858ccac995ec79543176c8239a3a4fa
Kevin D. Wolf <kevinw@software-logistics.com>
2026-08-27 17:08:28 -0400

    Move entity detail response contract beside implementation

---
+++ src/LagoVista.CloudStorage/Repositories/IEntityDetailResponseFactory.cs
@@ -0,0 +1,19 @@
+using LagoVista.Core.Models;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Threading.Tasks;
+
+namespace LagoVista.CloudStorage.Repositories
+{
+    public interface IEntityDetailResponseFactory
+    {
+        Task<JObject> CreateAiDetailResponseAsync(Type modelType, object model, EntityHeader org, EntityHeader user);
+        Task<JObject> CreateFormDetailResponseAsync(Type modelType, object model, EntityHeader org, EntityHeader user);
+        Task<JObject> GetAiDetailResponseAsync(string entityType, string id, EntityHeader org, EntityHeader user);
+        Task<JObject> GetFormDetailResponseAsync(string id, EntityHeader org, EntityHeader user);
+        Task<JObject> GetAiDetailResponseAsync(string id, EntityHeader org, EntityHeader user);
+        Task<JObject> GetFormDetailResponseAsync(string entityType, string id, EntityHeader org, EntityHeader user);
+        Task<(Type ModelType, object Model)> LoadModelAsync(string id, string requestedEntityType, EntityHeader user, EntityHeader org);
+        Task<(Type ModelType, object Model)> LoadModelAsync(string id, EntityHeader user, EntityHeader org);
+    }
+}
