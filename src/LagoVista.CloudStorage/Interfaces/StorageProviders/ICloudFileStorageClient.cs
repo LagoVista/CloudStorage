f8f146d4b50b959328b6a1429f35d20ca9c8f6b9
Kevin D. Wolf <kevinw@software-logistics.com>
2026-08-27 07:31:43 -0400

    Add cloud file storage provider interface

---
+++ src/LagoVista.CloudStorage/Interfaces/StorageProviders/ICloudFileStorageClient.cs
@@ -0,0 +1,25 @@
+using LagoVista.Core.Validation;
+using System;
+using System.Threading.Tasks;
+
+namespace LagoVista.CloudStorage.Interfaces
+{
+    public interface ICloudFileStorageClient
+    {
+        Task<InvokeResult<Uri>> AddFileAsync(string fileName, byte[] data, string contentType = "application/octet-stream", string cacheControl = null);
+
+        Task<InvokeResult<Uri>> AddFileAsync(string containerName, string fileName, byte[] data, string contentType = "application/octet-stream", string cacheControl = null, bool rejectUpdates = false);
+
+        Task<InvokeResult<Uri>> UpdateFileAsync(string containerName, string fileName, string data, string contentType = "text/plain", string cacheControl = null);
+
+        Task<InvokeResult<Uri>> AddFileAsync(string containerName, string fileName, string data, string contentType = "text/plain", string cacheControl = null);
+
+        Task<InvokeResult<byte[]>> GetFileAsync(string fileName);
+
+        Task<InvokeResult<byte[]>> GetFileAsync(string containerName, string fileName);
+
+        Task<InvokeResult> DeleteFileAsync(string fileName);
+
+        Task<InvokeResult> DeleteFileAsync(string containerName, string fileName);
+    }
+}
