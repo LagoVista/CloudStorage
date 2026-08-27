8b85b4b7e6ccadd6e0c70f043af007faa4ddf67f
Kevin D. Wolf <kevinw@software-logistics.com>
2026-08-27 17:52:28 -0400

    normalize provisioning cache namespace

--- src/LagoVista.CloudStorage/Storage/StorageProviders/DocumentCollectionProvisioningCache.cs
+++ src/LagoVista.CloudStorage/Storage/StorageProviders/DocumentCollectionProvisioningCache.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Concurrent;
 using System.Threading.Tasks;
 
-namespace LagoVista.CloudStorage.DocumentDB
+namespace LagoVista.CloudStorage.Storage.StorageProviders
 {
     public sealed class DocumentCollectionProvisioningCache
     {
