2c6b7513710cc11cea4659ad03c38c9daadd9065
Kevin D. Wolf <kevinw@software-logistics.com>
2026-09-23 11:28:58 -0400

    Return ListResponse from Mongo admin repository

--- src/LagoVista.CloudStorage/Interfaces/IMongoAdminRepo.cs
+++ src/LagoVista.CloudStorage/Interfaces/IMongoAdminRepo.cs
@@ -1,5 +1,5 @@
 using LagoVista.CloudStorage.Models;
-using System.Collections.Generic;
+using LagoVista.Core.Models.UIMetaData;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -7,9 +7,9 @@ namespace LagoVista.CloudStorage.Interfaces
 {
     public interface IMongoAdminRepo
     {
-        Task<IReadOnlyList<string>> GetDatabasesAsync(CancellationToken cancellationToken = default);
-        Task<IReadOnlyList<MongoCollectionInfo>> GetCollectionsAsync(string databaseName, CancellationToken cancellationToken = default);
-        Task<MongoQueryResult> QueryAsync(string databaseName, string collectionName, MongoQueryRequest request, CancellationToken cancellationToken = default);
+        Task<ListResponse<MongoDatabaseInfo>> GetDatabasesAsync(ListRequest listRequest, CancellationToken cancellationToken = default);
+        Task<ListResponse<MongoCollectionInfo>> GetCollectionsAsync(string databaseName, ListRequest listRequest, CancellationToken cancellationToken = default);
+        Task<ListResponse<MongoDocumentInfo>> QueryAsync(string databaseName, string collectionName, MongoQueryRequest request, CancellationToken cancellationToken = default);
         Task<string> GetDocumentAsync(string databaseName, string collectionName, string id, CancellationToken cancellationToken = default);
         Task<MongoPatchResult> PatchManyAsync(string databaseName, string collectionName, MongoPatchRequest request, CancellationToken cancellationToken = default);
     }
